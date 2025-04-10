98befb9638047afefe957ae73b53d32eb5bdde65
Ewelina <130902@gmail.com>
2025-04-10 21:05:25 +0200

    change in wait for element in first test for OrangeHRM login

--- src/test/java/part1/FirstTest.java
+++ src/test/java/part1/FirstTest.java
@@ -3,19 +3,25 @@ package part1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class FirstTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeClass
     public void setUp(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
     }
 
     @AfterClass
@@ -24,12 +30,11 @@ public class FirstTest {
     }
 
     @Test
-    public void testLoginIntoApplication() throws InterruptedException {
-        Thread.sleep(2000);
-      var username = driver.findElement(By.name("username"));
+    public void testLoginIntoApplication() {
+      var username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
       username.sendKeys("Admin");
 
-      var password = driver.findElement(By.name("password"));
+      var password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
       password.sendKeys("admin123");
 
       driver.findElement(By.tagName("button")).click();
